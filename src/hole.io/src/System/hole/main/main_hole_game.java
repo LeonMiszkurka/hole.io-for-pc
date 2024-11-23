1ac33a5acd8ffefe11a2664accc97f443d583562
Leon Miszkurka <leonmiszkurka@gmail.com>
2024-11-23 12:02:54 -0800

    adding main code

---
+++ src/hole.io/src/System/hole/main/main_hole_game.java
@@ -0,0 +1,23 @@
+package System.hole.main;
+
+import java.util.Scanner;
+import javax.swing.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class main_hole_game {
+    public static void main(String[] args) {
+        // makes the game scanner
+
+        System.out.println("welcome to hole.io");
+
+        JFrame FHJ1 = new JFrame("hole.io");
+        FHJ1.show();
+        JPanel PHJ1 = new JPanel();
+        FHJ1.add(PHJ1);
+        JButton STBHJ1 = new JButton("start game");
+        PHJ1.add(STBHJ1);
+        FHJ1.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+}
