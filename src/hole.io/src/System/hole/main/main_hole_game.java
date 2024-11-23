efd81a201cc154c1a138a9ac5a6b96642009948f
Leon Miszkurka <leonmiszkurka@gmail.com>
2024-11-23 12:19:52 -0800

    adding better app start

--- src/hole.io/src/System/hole/main/main_hole_game.java
+++ src/hole.io/src/System/hole/main/main_hole_game.java
@@ -2,6 +2,8 @@ package System.hole.main;
 
 import java.util.Scanner;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
@@ -18,6 +20,7 @@ public class main_hole_game {
         JButton STBHJ1 = new JButton("start game");
         PHJ1.add(STBHJ1);
         FHJ1.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        FHJ1.setSize(300. 200)
 
     }
 }
